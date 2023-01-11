b2e483721a31c569b50ab79de508ed5c660477c2
IT1089778 <nvrnagella@gmail.com>
2023-01-11 22:30:13 +0530

    priting color

--- print.sh
+++ print.sh
@@ -3,3 +3,8 @@ echo Hello world
 echo "Today is $(date), but my birthday is always 'May 15'"
 
 echo -e "\e[31mthis is enableing color\e[0m"
+echo -e "\e[32mthis is enableing color\e[0m"
+echo -e "\e[33mthis is enableing color\e[0m"
+echo -e "\e[34mthis is enableing color\e[0m"
+echo -e "\e[35mthis is enableing color\e[0m"
+echo -e "\e[36mthis is enableing color\e[0m"
