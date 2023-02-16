04c999d886492ec7dfefc36f0cdb4566a10d0cab
IT1089778 <nvrnagella@gmail.com>
2023-02-16 21:09:45 +0530

    added user

--- roboshop-shell/catalogue.sh
+++ roboshop-shell/catalogue.sh
@@ -4,7 +4,7 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 echo -e "\e[32m installing nodejs \e[0m"
 yum install nodejs -y
 echo -e "\e[33m adding roboshop user\e[0m"
-#useradd roboshop
+useradd roboshop
 echo -e "\e[34m creating app directory\e[0m"
 mkdir -p /app
 echo -e "\e[35m downloading catalogue content\e[0m"
